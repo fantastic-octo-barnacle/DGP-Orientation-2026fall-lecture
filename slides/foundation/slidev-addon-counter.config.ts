17260b2e16b0a58b65d97c8dbefba95da229ad20
Gyan <inaku.gs@gmail.com>
2026-09-17 17:47:13 +0800

    添加计数器配置的级别和格式

--- slides/foundation/slidev-addon-counter.config.ts
+++ slides/foundation/slidev-addon-counter.config.ts
@@ -1,5 +1,13 @@
 import { defineCounterConfig } from 'slidev-addon-counter/config'
 
 export default defineCounterConfig({
-  counters: [{ id: 'default', defaultLevel: 2 }],
+  counters: [{
+    id: 'default', defaultLevel: 2, levels: [{
+      "level": 1,
+      "format": "%{:value}."
+    }, {
+      "level": 2,
+      "format": "%{@-1:full}%{:value}"
+    }]
+  }],
 })
